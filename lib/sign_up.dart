2a8932a75839d6ad67ef3e48041489adb6520838
shfaysal <sazzadfaysal671@gmail.com>
2024-11-10 16:36:52 +0600

    color

--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -150,7 +150,7 @@ class _SignUpState extends State<SignUp> {
                       color: Colors.black
                     ),
                     decoration: const InputDecoration(
-                      labelText: "don't Your Name",
+                      labelText: "Enter Your Name",
                       labelStyle: TextStyle(
                         color: Colors.black,
                         fontSize: 20
